e547ba0ef161a69bb3563c2adc07cfef3cfe2180
Magnus Bøndergaard Poulsen <202008368@post.au.dk>
2024-02-09 12:07:18 +0100

    First solution assignement 2

--- My project/Assets/RayCast.cs
+++ My project/Assets/RayCast.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class RayCast : MonoBehaviour
 {
@@ -11,6 +13,12 @@ public class RayCast : MonoBehaviour
     [SerializeField] private Material myRed;
     [SerializeField] private Material myBlue;
     private Vector3 transformPosition;
+
+    public OVRHand leftHand;
+    private bool isLeftIndexPinching;
+    private bool hasSpawned = false;
+
+    public GameObject spawnSphere;
 
     // Start is called before the first frame update
     void Start()
@@ -30,12 +38,21 @@ public class RayCast : MonoBehaviour
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
         RaycastHit hit;
 
+        isLeftIndexPinching = leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index);
+
         if (Physics.Raycast(transform.position, fwd, out hit))
             print("There is something in front of the object! - " + " Pointer: " + hit.point);
         if (hit.transform.tag == "plane")
             transformPosition = childCursor.transform.position;
             transformPosition.x = hit.point.x;
             transformPosition.z = hit.point.z;
+
+        // Method for lefthand pinching
+        if (leftHand.IsTracked) // Checks if left hand is being tracked
+            if (isLeftIndexPinching && !hasSpawned) // Checks if a pinch is registred. And object is not already spawned. 
+                SpawnObject(transformPosition.x, transformPosition.y, transformPosition.z); // Calling spawn object function. 
+            else if (!isLeftIndexPinching && hasSpawned) // Else if released change hasSpawned to false again. 
+                hasSpawned = false;
 
         //Store last position
         childCursor.transform.position = transformPosition;
@@ -52,6 +69,15 @@ public class RayCast : MonoBehaviour
         }
 
 
+    }
+    // Spawn object method
+    void SpawnObject(float x, float y, float z)
+    {
+        Vector3 position = new Vector3(x, y,z); // Creates a new 3D Vector for the RayCast position. 
+        Instantiate(spawnSphere, position, Quaternion.identity); // Instaniates a gameObject based on spawnSPhere
+        MeshRenderer sphereMesh = spawnSphere.GetComponent<MeshRenderer>(); // Gets the mesh from the element
+        sphereMesh.material = myRed; // Changes it to red. 
+        hasSpawned = true;
     }
 }
    
